f560e42affd837e68097747e9c03078616d8a197
Brandon <brandoncyu@gmail.com>
2018-12-02 17:26:41 -0800

    finished reverseInt

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,22 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  let orig = n
+  let result = ''
+  let negative = false
+  if (n < 0){
+    n = n * -1
+    negative = true
+  }
+  let nString = n.toString()
+  if (nString.length === 1) return orig
+  for (character of nString){
+    result = character + result
+  }
+  let returnedResult = parseInt(result)
+  if (negative) returnedResult *= -1
+  return returnedResult
+}
 
 module.exports = reverseInt;
