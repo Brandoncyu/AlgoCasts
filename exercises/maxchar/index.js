d6714e9ec9a2170e805b2f3e5cb5bb1f4320afae
Brandon <brandoncyu@gmail.com>
2018-12-02 17:46:09 -0800

    finished maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,17 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let stack = {}
+
+  for (letter of str){
+    stack[letter] = stack[letter] + 1 || 1
+  }
+  let max = { 'mainKey': '', 'maxVal': 0 }
+  for (key in stack){
+    if (stack[key] > max['maxVal'] ) max = { 'mainKey': key, 'maxVal': stack[key]}
+  }
+  return max['mainKey']
+}
 
 module.exports = maxChar;
