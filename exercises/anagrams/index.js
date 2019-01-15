ff72e05cf5f8b3d597ba962472cf3ed64224cd26
Brandon <brandoncyu@gmail.com>
2019-01-15 15:49:43 -0800

    finished anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,37 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  const key = new Set(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'])
+  let result = true
+  let keyA = stringA.split('').reduce(function(accumulator, letter){
+    let lowLetter = letter.toLowerCase()
+    if (key.has(lowLetter) && !accumulator[lowLetter]){
+      accumulator[lowLetter] = 1
+    } else if (key.has(lowLetter) && accumulator[lowLetter]){
+      accumulator[lowLetter]++
+    }
+    return accumulator
+  }, {})
+  let keyB = stringB.split('').reduce(function(accumulator, letter){
+    let lowLetter = letter.toLowerCase()
+    if (key.has(lowLetter) && !accumulator[lowLetter]){
+      accumulator[lowLetter] = 1
+    } else if (key.has(lowLetter) && accumulator[lowLetter]){
+      accumulator[lowLetter]++
+    }
+    return accumulator
+  }, {})
+  console.log(keyA)
+  if (Object.keys(keyA).length !== Object.keys(keyB).length){
+    result = false
+  } else{
+    for(var newKey in keyA){
+      if (keyA[newKey] !== keyB[newKey]) result = false
+    }
+  }
+
+  return result
+}
 
 module.exports = anagrams;
